91ede2f7968dfd3499f488a963eb3d39a3f3347e
Cirilo LInares <rakode33@gmail.com>
2022-12-04 11:39:42 -0400

    Codificacion de actualizacion de actividades
    Actualizacion de actividades de una tarea

--- src/app/board/components/task/task.component.ts
+++ src/app/board/components/task/task.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { ListSchema, TaskSchema, Histori} from './../../../core';
 import { Actividades} from './../../../core/models/actividades';
+import { listaactividades} from './../../../core/models/listaactividades';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskService } from 'src/app/core/services/task.service';
 import { HistorialComponent } from '../historial/historial.component';
 import { EditActivityTaskComponent } from '../edit-activity-task/edit-activity-task.component';
 import {  CreateExcerciseTaskComponent } from '../create-excercise-task/create-excercise-task.component';
+import { IbartiService } from 'src/app/core/services/ibarti.service';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -20,10 +22,15 @@ export class TaskComponent implements OnInit {
   @Input() list?: ListSchema;
   @Input() users: Histori[] = [];
   panelOpenState = false;
-  
-  constructor(public dialog: MatDialog, public tasksService: TaskService) {}
+  actividades:listaactividades[]= [];
+  selectedactividad: string | undefined = "";
+  panelOpenStatedos = false;
+  constructor(public dialog: MatDialog, public tasksService: TaskService,private ibartiService: IbartiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getDataactividades();
+    this.selectedactividad = '';
+  }
 
   handleEditTask(task: TaskSchema) {
     
@@ -49,6 +56,23 @@ export class TaskComponent implements OnInit {
   handleExecersiTask(taskacti:Actividades): void {
     const dialogRef = this.dialog.open(CreateExcerciseTaskComponent, {
       data: {task: this.task,  taskacti: this.taskacti,listId: this.list?.codigo, users: this.users, panelClass: "panelModal"},
+    });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  getDataactividades(): void {
+   
+    this.ibartiService.getTasksEditActivity(this.task)
+      .subscribe(
+        (response: any) => this.actividades = response,
+        (error: string) => (console.log('Ups! we have an error: ', error))
+    );
+    
+  }
+  handleModificarctivityTask(task:TaskSchema): void {
+    const dialogRef = this.dialog.open(EditActivityTaskComponent, {
+      data: {task: this.task, listId: this.list?.codigo, users: this.users, panelClass: "panelModal"},
     });
     dialogRef.afterClosed().subscribe((result: any) => {
       console.log(`Dialog result: ${result}`);
---
+++ src/app/core/models/listaactividades.ts
@@ -0,0 +1,8 @@
+export interface listaactividades {
+    cod_nov_proc:string;
+    actividad:string;
+    fec_us_ing : string;
+    cod_us_mod: string;
+    usuario_mod	:string;
+        
+      }
